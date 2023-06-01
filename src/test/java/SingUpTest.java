ef3fb2020107408bf430e84dd11c3cd7b2a768a7
Victoria Shaitorova <shajtorova@mail.ru>
2023-06-01 19:53:10 +0300

    Initial commit1

--- src/test/java/SingUpTest.java
+++ src/test/java/SingUpTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 public class SingUpTest {
     @Test
+<<<<<<< HEAD
     public void zipCodeShouldAccept5Digits(){
         //1. перейти на https://www.sharelane.com/cgi-bin/register.py
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
@@ -36,10 +37,16 @@ public class SingUpTest {
     public void zipCodeShouldAccept4Digits(){
         //1. перейти на https://www.sharelane.com/cgi-bin/register.py
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+=======
+    public void zipCodeShouldAccept5Digits() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+>>>>>>> ee50298 (Initial commit1)
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
 
+<<<<<<< HEAD
         //2. ввести 1234
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys("1234");
@@ -58,4 +65,99 @@ public class SingUpTest {
         driver.quit();
 
     }
+=======
+
+        driver.findElement(By.name("zip_code"));
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("12345");
+
+
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+
+
+        WebElement registerButton = driver.findElement(By.cssSelector("[value=Register]"));
+        boolean isDisplayed = registerButton.isDisplayed();
+        Assert.assertTrue(isDisplayed, "Нужная страница не открылась");
+
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void enter4DigitsInTheZip_codeField() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+
+
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("1234");
+
+
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+
+
+        WebElement errorMessage = driver.findElement(By.cssSelector("[class=error_message]"));
+        String text = errorMessage.getText();
+        Assert.assertEquals(text, "Oops, error on page. ZIP code should have 5 digits", "Сообщение не отображается");
+
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void fillInTheRegistrationFieldsWithValidData() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+
+
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("12345");
+
+
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+
+
+        WebElement registerButton = driver.findElement(By.cssSelector("[value=Register]"));
+        boolean isDisplayed = registerButton.isDisplayed();
+        Assert.assertTrue(isDisplayed, "Нужная страница не открылась");
+
+
+        WebElement firstNameInput = driver.findElement(By.name("first_name"));
+        firstNameInput.sendKeys("Dima");
+
+        WebElement lastNameInput = driver.findElement(By.name("last_name"));
+        lastNameInput.sendKeys("Petrov");
+
+        WebElement emailInput = driver.findElement(By.name("email"));
+        emailInput.sendKeys("sssss@gmail.ru");
+
+        WebElement passwordInput = driver.findElement(By.xpath("//input [@name='password1']"));
+        passwordInput.sendKeys("88888");
+
+        WebElement confirmPasswordInput = driver.findElement(By.xpath("//input [@name='password2']"));
+        confirmPasswordInput.sendKeys("88888");
+
+        WebElement registerButtonInput = driver.findElement(By.cssSelector("[value=\"Register\"]"));
+        registerButtonInput.click();
+
+        WebElement registrationMessage = driver.findElement(By.xpath("//span[@class='confirmation_message']"));
+        String text1 = registrationMessage.getText();
+        Assert.assertEquals(text1, "Account is created!", "Пользователь не зарегистрировался");
+
+        driver.quit();
+
+
+    }
+>>>>>>> ee50298 (Initial commit1)
 }
