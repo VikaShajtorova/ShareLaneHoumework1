96a74e83bbb9a5e036a08a4c695ce5acc7f8166d
Victoria Shaitorova <shajtorova@mail.ru>
2023-06-03 12:01:20 +0300

    Initial commit2

--- src/test/java/SingUpTest.java
+++ src/test/java/SingUpTest.java
@@ -7,65 +7,14 @@ import org.testng.annotations.Test;
 
 public class SingUpTest {
     @Test
-<<<<<<< HEAD
-    public void zipCodeShouldAccept5Digits(){
-        //1. перейти на https://www.sharelane.com/cgi-bin/register.py
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
 
-        WebDriver driver= new ChromeDriver();
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-
-        //2. ввести 12345
-        driver.findElement(By.name("zip_code"));
-        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-        zipCodeInput.sendKeys("12345");
-
-        //3. нажать кнопку Continue
-        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
-        continueButton.click();
-
-        //4.убедиться что перешли на страницу ввода данных пользователя
-        WebElement registerButton = driver.findElement(By.cssSelector("[value=Register]"));
-        boolean isDisplayed = registerButton.isDisplayed();
-        Assert.assertTrue(isDisplayed,"Нужная страница не открылась");
-
-        //5. закрыть браузер
-        driver.quit();
-
-    }
-    @Test
-    public void zipCodeShouldAccept4Digits(){
-        //1. перейти на https://www.sharelane.com/cgi-bin/register.py
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
-=======
     public void zipCodeShouldAccept5Digits() {
 
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
->>>>>>> ee50298 (Initial commit1)
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
 
-<<<<<<< HEAD
-        //2. ввести 1234
-        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-        zipCodeInput.sendKeys("1234");
-
-        //3. нажать кнопку Continue
-        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
-        continueButton.click();
-
-        //4. убедиться что выдаст ошибку на экране
-        WebElement errorMessage = driver.findElement(By.cssSelector("[class=error_message]"));
-        errorMessage.getText();
-        String text = errorMessage.getText();
-        Assert.assertEquals(text,"Oops, error on page. ZIP code should have 5 digits","Сообщение не отображается");
-
-        //5. закрыть браузер
-        driver.quit();
-
-    }
-=======
 
         driver.findElement(By.name("zip_code"));
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
@@ -113,25 +62,19 @@ public class SingUpTest {
 
     @Test
     public void fillInTheRegistrationFieldsWithValidData() {
-
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
-
 
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys("12345");
 
-
         WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
         continueButton.click();
-
 
         WebElement registerButton = driver.findElement(By.cssSelector("[value=Register]"));
         boolean isDisplayed = registerButton.isDisplayed();
         Assert.assertTrue(isDisplayed, "Нужная страница не открылась");
-
 
         WebElement firstNameInput = driver.findElement(By.name("first_name"));
         firstNameInput.sendKeys("Dima");
@@ -159,5 +102,5 @@ public class SingUpTest {
 
 
     }
->>>>>>> ee50298 (Initial commit1)
+
 }
